2223293a0a266431c9b74a700d1779c3ed4af24a
dtaleme <kiwi0tails@gmail.com>
2022-10-28 20:47:36 +0300

    lab1

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,13 +1,22 @@
-#include "kernel.hpp" // note: unbalanced round brackets () are not allowed and string literals can't be arbitrarily long, so periodically interrupt with )+R(
-string opencl_c_container() { return R( // ########################## begin of OpenCL C code ####################################################################
+#include "kernel.hpp"  // note: unbalanced round brackets () are not allowed and string literals can't be arbitrarily long, so periodically interrupt with )+R(
+string opencl_c_container() {
+  return R(  // ########################## begin of OpenCL C code
+             // ####################################################################
 
+      kernel void kernel1() {  // equivalent to "for(uint n=0u; n<N; n++) {",
+                               // but executed in parallel
+        const uint N = get_group_id(0);
+        const uint M = get_local_id(0);
+        const uint K = get_global_id(0);
+        printf("block\t%d\tthread\t%d\tglobalindex\t%d\n", N, M, K);
+      }
 
+      kernel void kernel2(global float* A) {
+        const uint n = get_global_id(0);
+        A[n] = A[n] + n;
+      }
 
-kernel void add_kernel(global float* A, global float* B, global float* C) { // equivalent to "for(uint n=0u; n<N; n++) {", but executed in parallel
-	const uint n = get_global_id(0);
-	C[n] = A[n]+B[n];
-}
-
-
-
-);} // ############################################################### end of OpenCL C code #####################################################################
+  );
+}  // ############################################################### end of
+   // OpenCL C code
+   // #####################################################################
--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,36 @@
+#include <iomanip>
+
 #include "opencl.hpp"
 
 int main() {
-	Device device(select_device_with_most_flops()); // compile OpenCL C code for the fastest available device
+  Device device(
+      select_device_with_most_flops());  // compile OpenCL C code for the
+                                         // fastest available device
 
-	const uint N = 1024u; // size of vectors
-	Memory<float> A(device, N); // allocate memory on both host and device
-	Memory<float> B(device, N);
-	Memory<float> C(device, N);
+  const uint N = 20u;
+  const uint M = 5u;
 
-	Kernel add_kernel(device, N, "add_kernel", A, B, C); // kernel that runs on the device
+  Kernel kernel1(device, N, M, "kernel1");
+  std::cout << "\nTASK 1\n";
+  kernel1.run();
 
-	for(uint n=0u; n<N; n++) {
-		A[n] = 3.0f; // initialize memory
-		B[n] = 2.0f;
-		C[n] = 1.0f;
-	}
+  Memory<float> A(device, N);  // allocate memory on both host and device
+  Kernel kernel2(device, N, "kernel2", A);  // kernel that runs on the device
+  for (uint n = 0u; n < N; n++) {
+    A[n] = 0.0f;  // initialize memory
+  }
+  A.write_to_device();   // copy data from host memory to device memory
+  kernel2.run();         // run add_kernel on the device
+  A.read_from_device();  // copy data from device memory to host memory
 
-	print_info("Value before kernel execution: C[0] = "+to_string(C[0]));
+  std::cout << "\nTASK 2\n";
+  for (uint i = 0; i < N / M; i++) {
+    for (uint j = 0; j < M; j++) {
+      std::cout << std::left << std::setw(5) << A[i * (N / M) + j] << " ";
+    }
+    std::cout << "\n";
+  }
 
-	A.write_to_device(); // copy data from host memory to device memory
-	B.write_to_device();
-	add_kernel.run(); // run add_kernel on the device
-	C.read_from_device(); // copy data from device memory to host memory
-
-	print_info("Value after kernel execution: C[0] = "+to_string(C[0]));
-
-	wait();
-	return 0;
+  wait();
+  return 0;
 }
